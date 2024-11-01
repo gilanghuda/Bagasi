2fcaa81b4291202c7a8aa6d948d42cc5b0d8bbcc
gilang huda <gilanghuda99@gmail.com>
2024-11-01 23:08:45 +0700

    update

--- db/connectDB.js
+++ db/connectDB.js
@@ -3,12 +3,12 @@ const config = require("./config")
 
 let pool
 const connectDB = async () => {
-  pool ||= new Pool(config)
+  pool ||= new Pool({...config, max: 10 })
 
   try {
     const client = await pool.connect() 
     console.log("Connected to Bagasi database")
-    client.release() // hanya di release kalo client berhasil connect
+    // client.release() // hanya di release kalo client berhasil connect
   } catch (err) {
     console.error({ err }) 
   }
